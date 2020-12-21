7e3302955b0ffdaf2970753f2cc9fd89cbce3283
tonegawa07 <fukudayusuke07@gmail.com>
2020-12-21 21:13:46 +0900

    Vuetifyのレイアウト導入

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+import '@fortawesome/fontawesome-free/css/all.css' // Ensure you are using css-loader
 
 Vue.config.productionTip = false
 
